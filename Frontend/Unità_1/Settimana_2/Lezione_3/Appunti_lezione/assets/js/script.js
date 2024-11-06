14b698cea86a64367701264b9f02269caafc3f2b
Samuele Converso <samu.converso@gmail.com>
2024-11-06 14:20:35 +0100

    Appunti lezione S2 L3

---
+++ Frontend/Unità_1/Settimana_2/Lezione_3/Appunti_lezione/assets/js/script.js
@@ -0,0 +1,29 @@
+let num1 = 25;
+let num2 = 18;
+let num3 = 47;
+
+const today = new Date();
+console.log(today);
+
+const todayName = today.getDay();
+console.log(todayName);
+
+let i = 0;
+
+while (i < 10) {
+  console.log(i);
+  i++;
+}
+
+let numArr = [];
+let t = 0;
+
+while (t < 10) {
+  numArr[t] = t + 1;
+  t++;
+}
+console.log(numArr);
+
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
