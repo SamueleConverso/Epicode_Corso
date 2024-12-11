c12870d29ee6ba5a64e13a6c21c2bbf960e740d6
SamueleConverso <samu.converso@gmail.com>
2024-12-11 16:57:51 +0100

    .

---
+++ Frontend/Unità_2/Settimana_3/Lezione_3/Appunti_lezione/Esercizio/script.js
@@ -0,0 +1,44 @@
+const cards = document.querySelectorAll(".card");
+
+const url = "https://striveschool-api.herokuapp.com/books";
+let myMovies;
+
+function getData() {
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      myMovies = data;
+      console.log(myMovies);
+      printMovies();
+    })
+    .catch((error) => {
+      console.log("Errore: ", error);
+    });
+}
+
+document.addEventListener("load", init());
+
+function init() {
+  getData();
+}
+
+function printMovies() {
+  for (let i = 0; i < cards.length; i++) {
+    let image = cards[i].querySelector("img");
+    image.setAttribute("src", myMovies[i].img);
+
+    let price = cards[i].querySelector(".card-text:nth-of-type(2)");
+    price.innerText = `${myMovies[i].price}€`;
+  }
+}
+
+const btnClose = document.querySelectorAll(".btn-close-white");
+let arrBtnClose = Array.from(btnClose);
+
+arrBtnClose.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    console.log("Sono qui");
+  });
+});
