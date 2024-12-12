e87aeb1c598508ad8c150573982e7c479cf658fd
SamueleConverso <samu.converso@gmail.com>
2024-12-12 17:10:27 +0100

    .

---
+++ Frontend/Unità_2/Settimana_3/Lezione_4/Esercizio/assets/js/script.js
@@ -0,0 +1,73 @@
+const apiKey = "u50VDq1ZjwLEZtdTnhJN9DyOoQqocwzYPSgMeETmVxgCsQIUrAamzHsL";
+const cards = document.querySelectorAll(".card");
+const btnLoad = document.querySelector(".btn-primary");
+const btnLoad2 = document.querySelector(".btnLoad2");
+let data;
+let photos;
+let url = "https://api.pexels.com/v1/search?query=mountain&per_page=9";
+let query;
+
+
+async function getPhotos() {
+    try {
+        let response = await fetch(
+            createUrl(),
+            {
+                headers: {
+                    "Authorization":
+                        "u50VDq1ZjwLEZtdTnhJN9DyOoQqocwzYPSgMeETmVxgCsQIUrAamzHsL",
+                },
+            }
+        );
+        data = await response.json();
+        photos = data.photos;
+        printImage();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+//getPhotos();
+
+async function callGetPhotos(newQuery) {
+    query = newQuery;
+    try {
+        getPhotos();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+btnLoad.addEventListener("click", (e) => {
+    e.preventDefault();
+    callGetPhotos("mountain");
+});
+
+btnLoad2.addEventListener("click", (e) => {
+    e.preventDefault();
+    callGetPhotos("sunset");
+});
+
+function printImage() {
+    for (let i = 0; i < cards.length; i++) {
+        let image = cards[i].querySelector(".card-img-top");
+        //let image = document.querySelector(`${cards[i]} .card-img-top`);
+        image.setAttribute("src", photos[i].src.original);
+
+        let btn = cards[i].querySelector(".btn-outline-secondary:last-of-type");
+        btn.innerText = "Hide";
+        btn.classList.add(`${photos[i].id}`);
+        // btn.setAttribute("onclick", deleteCard());
+    }
+
+}
+
+function createUrl() {
+    if (!query) {
+        return "https://api.pexels.com/v1/search?query=mountain&per_page=9";
+    } else {
+        url = `https://api.pexels.com/v1/search?query=${query}&per_page=9`;
+        return url;
+    }
+
+}
