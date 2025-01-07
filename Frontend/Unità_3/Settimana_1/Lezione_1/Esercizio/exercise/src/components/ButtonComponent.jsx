d57fd12aec44f6969f5bfebc205317501d781aab
Samuele Converso <samu.converso@gmail.com>
2025-01-07 14:29:48 +0100

    .

---
+++ Frontend/Unità_3/Settimana_1/Lezione_1/Esercizio/exercise/src/components/ButtonComponent.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+function ButtonComponent(props) {
+  return (
+    <>
+      <button type="button">{props.btnName}</button>
+    </>
+  );
+}
+
+export default ButtonComponent;
