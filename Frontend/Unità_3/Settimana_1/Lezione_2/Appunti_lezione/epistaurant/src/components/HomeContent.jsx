7c29d3a018a777bf178f7cb09ca6010caada2547
Samuele Converso <samu.converso@gmail.com>
2025-01-08 14:04:08 +0100

    .

--- Frontend/Unità_3/Settimana_1/Lezione_2/Appunti_lezione/epistaurant/src/components/HomeContent.jsx
+++ Frontend/Unità_3/Settimana_1/Lezione_2/Appunti_lezione/epistaurant/src/components/HomeContent.jsx
@@ -1,29 +1,56 @@
-import { Carousel, Container, Row, Col } from "react-bootstrap";
+import { Carousel, Container, Row, Col, ListGroup } from "react-bootstrap";
+import pastasciutte from "../data/menu.json";
+import { Component } from "react";
 
-function HomeContent() {
-  return (
-    <>
-      <Container>
-        <Row className="justify-content-center">
-          <Col className="col-12 col-md-6">
-            <Carousel>
-              {}
+class HomeContent extends Component {
+  state = {
+    activePasta: pastasciutte[0],
+  };
 
-              <Carousel.Item>
-                <img src="https://placecats.com/300/300" className="w-100" />
-                <Carousel.Caption>
-                  <h3>First slide label</h3>
-                  <p>
-                    Nulla vitae elit libero, a pharetra augue mollis interdum.
-                  </p>
-                </Carousel.Caption>
-              </Carousel.Item>
-            </Carousel>
-          </Col>
-        </Row>
-      </Container>
-    </>
-  );
+  render() {
+    return (
+      <>
+        <Container>
+          <Row className="justify-content-center">
+            <Col className="col-12 col-md-6">
+              <Carousel
+                onSlide={(i) => {
+                  console.log("SLIDE CAMBIATA!", i);
+                  this.setState({
+                    activePasta: pastasciutte[i],
+                  });
+                }}
+              >
+                {pastasciutte.map((pasta) => {
+                  return (
+                    <Carousel.Item key={pasta.id}>
+                      <img src={pasta.image} className="w-100" />
+                      <Carousel.Caption>
+                        <h3>{pasta.name}</h3>
+                        <p>{pasta.description}</p>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+                  );
+                })}
+              </Carousel>
+            </Col>
+          </Row>
+
+          <Row className="justify-content-center">
+            <Col xs={12} md={6}>
+              <ListGroup className="text-center">
+                {this.state.activePasta.comments.map((r) => {
+                  return (
+                    <ListGroup.Item key={r.id}>{r.comment}</ListGroup.Item>
+                  );
+                })}
+              </ListGroup>
+            </Col>
+          </Row>
+        </Container>
+      </>
+    );
+  }
 }
 
 export default HomeContent;
