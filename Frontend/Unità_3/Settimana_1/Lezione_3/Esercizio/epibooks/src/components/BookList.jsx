16777d4316c4781053351dd9c844c6fb14de0e64
Samuele Converso <samu.converso@gmail.com>
2025-01-09 15:29:06 +0100

    .

--- Frontend/Unità_3/Settimana_1/Lezione_3/Esercizio/epibooks/src/components/BookList.jsx
+++ Frontend/Unità_3/Settimana_1/Lezione_3/Esercizio/epibooks/src/components/BookList.jsx
@@ -1,17 +1,13 @@
 import SingleBook from "./SingleBook";
 import books from "../data/fantasy.json";
-import { Col, Container } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 function BookList() {
   return (
     <>
       <Container className="d-flex flex-wrap justify-content-center gap-3">
         {books.map((book) => {
-          return (
-            <Col key={book.asin}>
-              <SingleBook book={book} />
-            </Col>
-          );
+          return <SingleBook key={book.asin} book={book} />;
         })}
       </Container>
     </>
