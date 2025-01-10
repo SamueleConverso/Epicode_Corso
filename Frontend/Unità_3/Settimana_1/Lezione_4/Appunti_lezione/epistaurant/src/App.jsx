c9fdd0e11d13118c279371020c651610feb8871f
Samuele Converso <samu.converso@gmail.com>
2025-01-10 11:20:37 +0100

    .

--- Frontend/Unità_3/Settimana_1/Lezione_4/Appunti_lezione/epistaurant/src/App.jsx
+++ Frontend/Unità_3/Settimana_1/Lezione_4/Appunti_lezione/epistaurant/src/App.jsx
@@ -3,6 +3,7 @@
 //import viteLogo from "/vite.svg";
 //import "./App.css";
 import RestaurantNavbar from "./components/RestaurantNavbar";
+import AdminSection from "./components/AdminSection";
 import ReservationForm from "./components/ReservationForm";
 import HomeContent from "./components/HomeContent";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
       <RestaurantNavbar />
+      <AdminSection />
       <ReservationForm />
       <HomeContent />
     </>
---
+++ Frontend/Unità_3/Settimana_1/Lezione_4/Appunti_lezione/epistaurant/src/components/AdminSection.jsx
@@ -0,0 +1,73 @@
+import { Component } from "react";
+import { Container, Row, Col, ListGroup } from "react-bootstrap";
+
+const URL = "https://striveschool-api.herokuapp.com/api/reservation";
+
+class AdminSection extends Component {
+  state = {
+    reservations: [],
+  };
+
+  getReservationsThenCatch = () => {
+    fetch(URL)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Errore nella chiamata API");
+        }
+      })
+      .then((data) => {
+        console.log("DATA", data);
+      })
+      .catch((error) => {
+        console.log("Errore di connessione o altro", error);
+      });
+  };
+
+  getReservationsAsyncAwait = async () => {
+    try {
+      let response = await fetch(URL);
+      if (response.ok) {
+        let data = await response.json();
+        console.log("DATA", data);
+        this.setState({
+          reservations: data,
+        });
+      } else {
+        throw new Error("Errore nel recupero dati API.");
+      }
+    } catch (error) {
+      console.log("ERRORE", error);
+    }
+  };
+
+  render() {
+    return (
+      <Container>
+        <Row className="justify-content-center my-3">
+          <Col xs={12} md={6}>
+            <div>
+              <h2 className="text-center">Prenotazioni esistenti</h2>
+            </div>
+          </Col>
+        </Row>
+        <Row className="justify-content-center my-3">
+          <Col xs={12} md={6}>
+            <ListGroup>
+              {this.state.reservations.map((res) => {
+                return (
+                  <ListGroup.Item key={res._id}>
+                    {res.name} per {res.numberOfPeople}
+                  </ListGroup.Item>
+                );
+              })}
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default AdminSection;
