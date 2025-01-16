fac4f78cad1e78fa55ce982ca0363aa6f23eb7c1
Samuele Converso <samu.converso@gmail.com>
2025-01-16 16:30:30 +0100

    .

--- Frontend/Unità_3/Settimana_2/Lezione_4/Esercizio/epibooks-3/src/tests/SingleBook.test.jsx
+++ Frontend/Unità_3/Settimana_2/Lezione_4/Esercizio/epibooks-3/src/tests/SingleBook.test.jsx
@@ -1,7 +1,7 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import SingleBook from "../components/SingleBook";
-import BookList from "../components/BookList";
+//import BookList from "../components/BookList";
 import books from "../data/fantasy.json";
 
 describe("Testing border", () => {
