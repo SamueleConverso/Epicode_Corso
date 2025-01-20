f759905a9b83c8bafd87ad912eaf259053ab7ae5
Samuele Converso <samu.converso@gmail.com>
2025-01-20 13:01:29 +0100

    .

--- Frontend/Unità_3/Settimana_3/Lezione_1/Appunti_lezione/epizon/src/components/BookDetail.jsx
+++ Frontend/Unità_3/Settimana_3/Lezione_1/Appunti_lezione/epizon/src/components/BookDetail.jsx
@@ -3,6 +3,7 @@ import { FaShoppingCart } from "react-icons/fa";
 import { useDispatch } from "react-redux";
 
 const BookDetail = ({ bookSelected }) => {
+  const dispatch = useDispatch();
   return (
     <div className="mt-3 mb-4 mb-lg-0">
       {bookSelected ? (
@@ -31,7 +32,12 @@ const BookDetail = ({ bookSelected }) => {
                 <span className="fw-bold">Price:</span>&nbsp;
                 {bookSelected.price}$
               </p>
-              <Button className="d-flex align-items-center" onClick={() => {}}>
+              <Button
+                className="d-flex align-items-center"
+                onClick={() => {
+                  dispatch({ type: "ADD_TO_CART", payload: bookSelected });
+                }}
+              >
                 <span className="me-2">AGGIUNGI AL</span>
                 <FaShoppingCart />
               </Button>
--- Frontend/Unità_3/Settimana_3/Lezione_1/Appunti_lezione/epizon/src/redux/reducers/index.js
+++ Frontend/Unità_3/Settimana_3/Lezione_1/Appunti_lezione/epizon/src/redux/reducers/index.js
@@ -6,6 +6,30 @@ const initialState = {
 
 function mainReducer(state = initialState, action) {
   switch (action.type) {
+    case "ADD_TO_CART":
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          content: state.cart.content.concat(action.payload),
+        },
+      };
+
+    case "REMOVE_FROM_CART":
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          content: state.cart.content.filter((book, i) => {
+            // if (action.payload === i) {
+            //   return false;
+            // } else {
+            //   return true;
+            // }
+            return action.payload !== i;
+          }),
+        },
+      };
     default:
       return state;
   }
