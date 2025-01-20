af33a8641e180e833355e0fe69dc5bb4f4be0582
Samuele Converso <samu.converso@gmail.com>
2025-01-20 16:48:41 +0100

    .

--- Frontend/Unità_3/Settimana_3/Lezione_1/Esercizio/job-searcher/src/components/Favourites.jsx
+++ Frontend/Unità_3/Settimana_3/Lezione_1/Esercizio/job-searcher/src/components/Favourites.jsx
@@ -1,19 +1,42 @@
 import { useSelector } from "react-redux";
-import { ListGroup } from "react-bootstrap";
+import { Button, ListGroup, Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 function Favourites() {
   const favJobs = useSelector((state) => {
     return state.favourites.jobs;
   });
 
+  const dispatch = useDispatch();
+
   return (
-    <ListGroup className="text-center mt-5">
+    <ListGroup>
       {favJobs.map((job, i) => {
         return (
-          <Link key={i} to={`/${job}`}>
-            <ListGroup.Item>{job}</ListGroup.Item>
-          </Link>
+          <Container key={i}>
+            <Row className="justify-content-center align-items-center">
+              <Col xs={6} className="mt-2">
+                <Link to={`/${job}`}>
+                  <ListGroup.Item className="text-center">{job}</ListGroup.Item>
+                </Link>
+              </Col>
+              <Col xs={6} className="d-flex justify-content-center">
+                <Button
+                  style={{ width: "fit-content" }}
+                  variant="danger"
+                  onClick={() => {
+                    dispatch({
+                      type: "REMOVE_FROM_FAVOURITES",
+                      payload: job,
+                    });
+                  }}
+                >
+                  🗑️
+                </Button>
+              </Col>
+            </Row>
+          </Container>
         );
       })}
     </ListGroup>
