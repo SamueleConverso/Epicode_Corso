0b095250aea6d5c5d18620426f171fb698dfcf81
Samuele Converso <samu.converso@gmail.com>
2025-01-23 09:43:16 +0100

    .

---
+++ Frontend/Unità_3/Settimana_3/Lezione_4/Appunti_lezione/ts-react-demo/src/components/ClassComponent.tsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class ClassComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Componente a classe!</h1>
+        <h3>Valore prop</h3>
+      </div>
+    );
+  }
+}
+
+export default ClassComponent;
